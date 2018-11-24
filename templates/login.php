5d4917661c5ec6292773ed7eca4a94c8f0e9bbcb
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2018-11-24 12:04:50 -0800

    Import

--- templates/login.php
+++ templates/login.php
@@ -7,126 +7,96 @@
 		<meta name="author" content="">
 		<title>Pricing example for Bootstrap</title>
 		<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
-		<!--<link href="/assets/css/jumbotron.css" rel="stylesheet">-->
 		<link href="/assets/css/multidata.css" rel="stylesheet">
+		<link href="/assets/css/pricing.css" rel="stylesheet">
 	</head>
 	<body>
 
-<nav class="navbar navbar-default navbar-expand-md navbar-static-top" style="margin-bottom: 0">
-	<div class="mx-auto order-0">
-		<a class="navbar-brand" href="/">
-      <img width="auto" style="max-height:30px;vertical-align:middle;" src="/assets/imgs/icon_multidata.jpg">
-      <b>MultiDATA</b>
-    </a>
-	</div>
-	<div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-		<ul class="nav navbar-top-links mr-auto">
-			<li class="nav-item active">
-				<a class="nav-link" href="#">Principal</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Nosotros</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="https://OpenDataCollector.com">API (OpenDataCollector)</a>
-			</li>
-		</ul>
-	</div>
-	<div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-		<ul class="nav navbar-top-links ml-auto">
-			<li class="nav-item" style="vertical-align: middle;">
-				<font color="#ffffff">Ingresar:</font>
-			</li>
-			<li class="nav-item">
-				<a class="btn btn-light btn-space" id="btn_google" href="/login/google">
-					<img src="/assets/imgs/google.png" width="20"> Google
-				</a>
-			</li>
-			<li class="nav-item">
-				<a class="btn btn-light btn-space" id="btn_facebook" href="/login/facebook">
-					<img src="/assets/imgs/facebook.png" width="22"> Facebook
-				</a>
-			</li>
-		</ul>
-	</div>
-</nav>
+		<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+			<h5 class="my-0 mr-md-auto font-weight-normal">MultiDATA</h5>
+       <nav class="my-2 my-md-0 mr-md-3">
+         <a class="p-2 text-dark" href="/">Principal</a>
+         <a class="p-2 text-dark" href="#">Proyecto</a>
+         <a class="p-2 text-dark" target="_blank" href="https://OpenDataCollector.com">API (OpenDataCollector)</a>
+       </nav>
+       <a class="btn btn-outline-primary" href="#">Ingresar</a>
+     </div>
 
-		<main role="main">
+     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+       <h1 class="display-4">Pricing</h1>
+       <p class="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p>
+     </div>
 
-			<div class="jumbotron">
-				<div class="container">
-					<h1 class="display-3">Hello, world!</h1>
-					<p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-					<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
-				</div>
-			</div>
+     <div class="container">
+       <div class="card-deck mb-3 text-center">
+         <div class="card mb-4 shadow-sm">
+           <div class="card-header text-white bg-primary">
+             <h4 class="my-0 font-weight-normal">Veh&iacute;culos</h4>
+           </div>
+           <div class="card-body">
+             <h2 class="card-title pricing-card-title">Informaci&oacute;n</h2>
+						 <ul class="list-styled mt-3 mb-4">
+               <li>Datos veh&iacute;culo</li>
+               <li>Transporte p&uacute;blico</li>
+               <li>Registro policial</li>
+							 <li>Propietarios</li>
+             </ul>
+           </div>
+					 <div class="card-footer bg-transparent border-primary">
+						 <button type="button" class="btn btn-md btn-block btn-success">Realizar busqueda</button>
+					 </div>
+         </div>
+         <div class="card mb-4 shadow-sm">
+           <div class="card-header">
+             <h4 class="my-0 font-weight-normal">Personas</h4>
+           </div>
+           <div class="card-body">
+						 <h2 class="card-title pricing-card-title">Informaci&oacute;n</h2>
+						 <ul class="list-styled mt-3 mb-4">
+               <li>Registro electoral</li>
+               <li>Informaci&oacute;n tributaria</li>
+               <li>Gobierno transparente</li>
+             </ul>
+           </div>
+					 <div class="card-footer bg-transparent border-primary">
+						 <button type="button" class="btn btn-md btn-block btn-outline-success">Pronto</button>
+					 </div>
+         </div>
+         <div class="card mb-4 shadow-sm">
+           <div class="card-header">
+             <h4 class="my-0 font-weight-normal">Sociedades</h4>
+           </div>
+           <div class="card-body">
+						 <h2 class="card-title pricing-card-title">Informaci&oacute;n</h2>
+						 <ul class="list-styled mt-3 mb-4">
+               <li>Informaci&oacute;n tributaria</li>
+               <li>Mercado y licitaciones</li>
+							 <li>Previsional y laboral</li>
+							 <li>Gobierno transparente</li>
+             </ul>
+           </div>
+					 <div class="card-footer bg-transparent border-primary">
+						 <button type="button" class="btn btn-md btn-block btn-outline-success">Pronto</button>
+					 </div>
+         </div>
+       </div>
 
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4">
+			 <footer class="footer">
+				 <hr>
+	       <div class="row py-4 d-flex align-items-center">
+	         <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+	           <b>MultiDATA.CL</b> &copy; by <a href="http://xpertians.com">Xpertians</a><br>
+						 <a href="https://odc.xpertians.com/docs/terminos-de-servicio/">Condiciones</a> &bull;
+						 <a href="https://OpenDataCollector.com">API (OpenDataCollector)</a>
+	         </div>
+	         <div class="col-md-6 col-lg-7 text-center text-md-right">
+	 					Software Libre bajo licencia <a target="_blank" href="https://github.com/Xpertians/multidatacl/blob/master/LICENSE">GPL-3.0</a> <br>
+						<a target="_blank" href="https://github.com/Xpertians/multidatacl">Fork en GitHub</a>
+	         </div>
+	       </div>
+	 		</footer>
 
-						<div class="card" style="width: 18rem;">
-							<div class="card-header bg-warning text-white">
-								<h2>Veh&iacute;culos</h2>
-							</div>
-							<div class="card-footer">
-					      <a href="#" class="btn btn-warning text-white btn-sm float-right">Realizar busqueda</a>
-					    </div>
-						</div>
-
-
-					</div>
-					<div class="col-md-4">
-
-						<div class="card" style="width: 18rem;">
-							<div class="card-header bg-success text-white">
-								<h2>Personas</h2>
-							</div>
-							<div class="card-footer">
-								<a href="#" class="btn btn-success text-white btn-sm float-right">Realizar busqueda</a>
-					    </div>
-						</div>
-
-					</div>
-					<div class="col-md-4">
-
-						<div class="card" style="width: 18rem;">
-							<div class="card-header bg-primary text-white">
-								<h2>Empresas</h2>
-							</div>
-							<div class="card-footer">
-								<a href="#" class="btn btn-primary text-white btn-sm float-right">Realizar busqueda</a>
-					    </div>
-						</div>
-
-
-					</div>
-				</div>
-				<hr>
-				<?php
-				$identifier_session = !empty( Hybrid_Auth::storage() ) ? Hybrid_Auth::storage()->get( 'user' ) : null;
-				if (isset( $identifier_session ) && ! empty( $identifier_session )) {
-					echo '<a href="/welcome">Return to Control Panel</a>';
-				}
-
-				if(isset($_GET['err']) && !empty($_GET['err'])) {
-				    echo '<div>Authentication failed. Please try again</div>';
-				}
-				?>
-			</div> <!-- /container -->
-
-		</main>
-
-		<footer class="container">
-      <div class="row py-4 d-flex align-items-center">
-        <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
-          <b>MultiDATA.CL</b> &copy; by <a href="http://xpertians.com">Xpertians</a>
-        </div>
-        <div class="col-md-6 col-lg-7 text-center text-md-right">
-					Software Libre bajo licencia GPL-3.0 - <a href="#">Descargar</a>
-        </div>
-      </div>
-		</footer>
+     </div>
 
 		<script src="/assets/js/jquery-3.3.1.slim.min.js"></script>
 		<script src="/assets/js/bootstrap.min.js"></script>
