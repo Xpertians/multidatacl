c9e2ed0b58cc1ec428a1ab47265b933c9b454cbd
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2018-11-23 23:06:51 -0800

    x

--- templates/login.php
+++ templates/login.php
@@ -15,50 +15,36 @@
 
   <body>
 
-		<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-		      <a class="navbar-brand" href="#">Navbar</a>
-		      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-		        <span class="navbar-toggler-icon"></span>
-		      </button>
+	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+	      <a class="navbar-brand" href="#">Navbar</a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="navbar-toggler-icon"></span>
+	      </button>
 
-		      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-		        <ul class="navbar-nav mr-auto">
-		          <li class="nav-item active">
-		            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-		          </li>
-		          <li class="nav-item">
-		            <a class="nav-link" href="#">Link</a>
-		          </li>
-		          <li class="nav-item">
-		            <a class="nav-link disabled" href="#">Disabled</a>
-		          </li>
-		          <li class="nav-item dropdown">
-		            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-		            <div class="dropdown-menu" aria-labelledby="dropdown01">
-		              <a class="dropdown-item" href="#">Action</a>
-		              <a class="dropdown-item" href="#">Another action</a>
-		              <a class="dropdown-item" href="#">Something else here</a>
-		            </div>
-		          </li>
-		        </ul>
-		        <form class="form-inline my-2 my-lg-0">
-		          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-		          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-		        </form>
-						<a class="btn btn-outline-primary" href="/login/google">Sign up</a>
-		      </div>
-		    </nav>
-
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-      <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
-      <nav class="my-2 my-md-0 mr-md-3">
-        <a class="p-2 text-dark" href="#">Features</a>
-        <a class="p-2 text-dark" href="#">Enterprise</a>
-        <a class="p-2 text-dark" href="#">Support</a>
-        <a class="p-2 text-dark" href="#">Pricing</a>
-      </nav>
-      <a class="btn btn-outline-primary" href="/login/google">Sign up</a>
-    </div>
+	      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+	        <ul class="navbar-nav mr-auto">
+	          <li class="nav-item active">
+	            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+	          </li>
+	          <li class="nav-item">
+	            <a class="nav-link" href="#">Link</a>
+	          </li>
+	          <li class="nav-item">
+	            <a class="nav-link disabled" href="#">Disabled</a>
+	          </li>
+	          <li class="nav-item dropdown">
+	            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+	            <div class="dropdown-menu" aria-labelledby="dropdown01">
+	              <a class="dropdown-item" href="#">Action</a>
+	              <a class="dropdown-item" href="#">Another action</a>
+	              <a class="dropdown-item" href="#">Something else here</a>
+	            </div>
+	          </li>
+	        </ul>
+	        Login with
+					<a class="btn btn-primary" href="/login/google">Sign up</a>
+	      </div>
+	    </nav>
 
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
       <h1 class="display-4">Pricing</h1>
