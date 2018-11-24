5c353dd4102f1115062862a60747ebb6588c71c9
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2018-11-24 14:42:30 -0800

    fix

--- templates/login.php
+++ templates/login.php
@@ -13,16 +13,15 @@
 	<body>
 
 		<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-			<h4 class="my-0 mr-md-auto font-weight-normal" style="color:#1B3C81;">
-				<a href="/"><img src="/assets/imgs/multidata.png" width='80px'></a>
-				<b>MultiDATA</b>
+			<h4 class="my-0 mr-md-auto font-weight-normal">
+				<a href="/"><img src="/assets/imgs/multidata.png" height='60px'></a>
 			</h4>
        <nav class="my-2 my-md-0 mr-md-3">
          <a class="p-2 text-dark" href="/">Principal</a>
          <a class="p-2 text-dark" href="#">Proyecto</a>
          <a class="p-2 text-dark" target="_blank" href="https://OpenDataCollector.com">API (OpenDataCollector)</a>
        </nav>
-       <a class="btn btn-outline-primary" href="#">Ingresar</a>
+       <a class="btn btn-outline-primary active" href="#">Ingresar</a>
      </div>
 
      <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
