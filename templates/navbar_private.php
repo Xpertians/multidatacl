8c85ba8d7024f91c8194333ede1479c826c11df3
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2018-11-28 01:51:31 +0000

    Fix

---
+++ templates/navbar_private.php
@@ -0,0 +1,11 @@
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+		<h4 class="my-0 mr-md-auto font-weight-normal">
+			<a href="/"><img src="/assets/imgs/multidata.png" height='60px'></a>
+		</h4>
+		<nav class="my-2 my-md-0 mr-md-3">
+			<a class="p-2 text-dark" href="/">Bienvenid@: <?=$model->getFirstName( $identifier );?> <?=$model->getLastName( $identifier );?></a>
+		</nav>
+		<button type="button" class="btn btn-outline-danger" onClick="window.location.href='/logout';">
+			Salir
+		</button>
+	</div>
---
+++ templates/navbar_public.php
@@ -0,0 +1,13 @@
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+		<h4 class="my-0 mr-md-auto font-weight-normal">
+			<a href="/"><img src="/assets/imgs/multidata.png" height='60px'></a>
+		</h4>
+		<nav class="my-2 my-md-0 mr-md-3">
+			<a class="p-2 text-dark" href="/home">Principal</a>
+			<a class="p-2 text-dark" href="#">Proyecto</a>
+			<a class="p-2 text-dark" target="_blank" href="https://OpenDataCollector.com">API (OpenDataCollector)</a>
+		</nav>
+		<button type="button" class="btn btn-outline-primary active" data-toggle="modal" data-target="#sem-login">
+			Ingresar
+		</button>
+	</div>
