3b16dd03f936ec76918ea06771950553549dca08
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2018-11-28 02:04:15 +0000

    Fix

--- templates/navbar_private.php
+++ templates/navbar_private.php
@@ -2,9 +2,9 @@
 		<h4 class="my-0 mr-md-auto font-weight-normal">
 			<a href="/"><img src="/assets/imgs/multidata.png" height='60px'></a>
 		</h4>
-		<img src="<?=$model->getAvatarUrl( $identifier );?>" height="20px;">
+		<img src="<?=$model->getAvatarUrl( $identifier_session );?>" height="20px;">
 		<nav class="my-2 my-md-0 mr-md-3 text-left">
-			<a class="p-2 text-dark" href="#"><b>Bienvenid@</b> <br><?=$model->getFirstName( $identifier );?> <?=$model->getLastName( $identifier );?></a>
+			<a class="p-2 text-dark" href="#"><b>Bienvenid@</b> <br><?=$model->getFirstName( $identifier_session );?> <?=$model->getLastName( $identifier_session );?></a>
 		</nav>
 		<button type="button" class="btn btn-outline-danger" onClick="window.location.href='/logout';">
 			Desconectar
