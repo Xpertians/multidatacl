097054d9aa76d91d92bd2d7e8a19c8f2a0c0ef90
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2018-11-28 02:01:52 +0000

    Fix

--- templates/navbar_private.php
+++ templates/navbar_private.php
@@ -2,7 +2,7 @@
 		<h4 class="my-0 mr-md-auto font-weight-normal">
 			<a href="/"><img src="/assets/imgs/multidata.png" height='60px'></a>
 		</h4>
-		<img src="<?=$model->getAvatarUrl( $identifier );?>" height="20px">
+		<img src="" height="20px">
 		<nav class="my-2 my-md-0 mr-md-3 text-left">
 			<a class="p-2 text-dark" href="#"><b>Bienvenid@</b> <br><?=$model->getFirstName( $identifier );?> <?=$model->getLastName( $identifier );?></a>
 		</nav>
