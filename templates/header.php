c1f43c1dca8ef08cba625ee5cc66156afa058898
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2018-11-28 01:33:21 +0000

    Fix

---
+++ templates/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>MultiDATA.CL</title>
+		<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+		<link href="/assets/css/multidata.css" rel="stylesheet">
+		<link href="/assets/css/pricing.css" rel="stylesheet">
+	</head>
+	<body>
--- templates/login.php
+++ templates/login.php
@@ -1,19 +1,7 @@
 <?php
 $identifier_session = !empty( Hybrid_Auth::storage() ) ? Hybrid_Auth::storage()->get( 'user' ) : null;
+include_once("header.php");
 ?>
-<!doctype html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="description" content="">
-		<meta name="author" content="">
-		<title>MultiDATA.CL</title>
-		<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
-		<link href="/assets/css/multidata.css" rel="stylesheet">
-		<link href="/assets/css/pricing.css" rel="stylesheet">
-	</head>
-	<body>
 		 <?php
 		 if(isset($_GET['err']) && !empty($_GET['err'])) {
 		     echo '<div>Authentication failed. Please try again</div>';
@@ -133,35 +121,7 @@ $identifier_session = !empty( Hybrid_Auth::storage() ) ? Hybrid_Auth::storage()-
 					 </div>
          </div>
        </div>
-
-			 <footer class="footer">
-				 <hr>
-	       <div class="row py-4 d-flex align-items-center">
-	         <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
-	           <a href="https://odc.xpertians.com/docs/terminos-de-servicio/">Condiciones</a> &bull;
-						 <a href="https://OpenDataCollector.com">API (OpenDataCollector)</a><br>
-						 <b>MultiDATA.CL</b> &copy; by <a href="http://xpertians.com">Xpertians</a>
-	         </div>
-	         <div class="col-md-6 col-lg-7 text-center text-md-right">
-	 					Puedes descargar el c&oacute;digo en <a target="_blank" href="https://github.com/Xpertians/multidatacl">GitHub</a><br>
-						Software Libre bajo licencia <a target="_blank" href="https://github.com/Xpertians/multidatacl/blob/master/LICENSE">GPL-3.0</a>
-	         </div>
-	       </div>
-	 		</footer>
-
-     </div>
-
-		<script src="/assets/js/jquery-3.3.1.slim.min.js"></script>
-		<script src="/assets/js/bootstrap.min.js"></script>
-		<script src="/assets/js/vendor/popper.min.js"></script>
-		<script src="/assets/js/vendor/holder.min.js"></script>
-		<script>
-			Holder.addTheme('thumb', {
-			bg: '#55595c',
-			fg: '#eceeef',
-			text: 'Thumbnail'
-			});
-		</script>
-
-	</body>
-</html>
+       
+<?php
+include_once("footer.php");
+?>
