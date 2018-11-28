6d2084d252d5e764a9b024b91808e1fa3552bebf
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2018-11-28 21:02:48 +0000

    Pushing new code

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInitabc0d7356e94fb32083aeb18c9a333a3
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitabc0d7356e94fb32083aeb18c9a333a3::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequireabc0d7356e94fb32083aeb18c9a333a3($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequireabc0d7356e94fb32083aeb18c9a333a3($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
