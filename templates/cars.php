5d1115f4c9e26ca34b81c85e7e5e4dcb79a1d25a
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2018-12-03 10:47:28 +0000

    Fix

--- templates/cars.php
+++ templates/cars.php
@@ -13,7 +13,7 @@ if(isset($_GET['err']) && !empty($_GET['err'])) {
 ?>
 
      <div class="container">
-       <div class="card-deck mb-2 text-center">
+       <div class="card-deck mb-3 text-center">
          <div class="card mb-4 shadow-sm">
            <div class="card-header text-white bg-warning">
              <h4 class="my-0 font-weight-normal">Veh&iacute;culos</h4>
@@ -35,16 +35,16 @@ if(isset($_GET['err']) && !empty($_GET['err'])) {
              <h4 class="my-0 font-weight-normal">Personas</h4>
            </div>
            <div class="card-body">
-						 <h2 class="card-title pricing-card-title">Informaci&oacute;n</h2>
-						 <ul class="list-styled mt-3 mb-4">
+			 <h2 class="card-title pricing-card-title">Informaci&oacute;n</h2>
+			 <ul class="list-styled mt-3 mb-4">
                <li>Registro electoral</li>
                <li>Informaci&oacute;n tributaria</li>
                <li>Gobierno transparente</li>
              </ul>
            </div>
-					 <div class="card-footer bg-transparent border-success">
-						 <button type="button" class="btn btn-md btn-block btn-outline-success">Pronto</button>
-					 </div>
+			 <div class="card-footer bg-transparent border-success">
+				 <button type="button" class="btn btn-md btn-block btn-outline-success">Pronto</button>
+			 </div>
          </div>
        </div>
        
