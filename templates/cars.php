a994453e0920bd066f28bba0d28dce295149128b
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2018-12-03 10:46:49 +0000

    Fix

--- templates/cars.php
+++ templates/cars.php
@@ -13,7 +13,7 @@ if(isset($_GET['err']) && !empty($_GET['err'])) {
 ?>
 
      <div class="container">
-       <div class="card-deck mb-3 text-center">
+       <div class="card-deck mb-2 text-center">
          <div class="card mb-4 shadow-sm">
            <div class="card-header text-white bg-warning">
              <h4 class="my-0 font-weight-normal">Veh&iacute;culos</h4>
@@ -30,7 +30,7 @@ if(isset($_GET['err']) && !empty($_GET['err'])) {
              <button type="button" class="btn btn-md btn-block btn-warning" onClick="window.location.href='/home/cars';">Buscar</button>
             </div>
          </div>
-         <div class="card mb-4 shadow-sm">
+         <div class="card mb-8 shadow-sm">
            <div class="card-header text-white bg-success">
              <h4 class="my-0 font-weight-normal">Personas</h4>
            </div>
@@ -44,23 +44,6 @@ if(isset($_GET['err']) && !empty($_GET['err'])) {
            </div>
 					 <div class="card-footer bg-transparent border-success">
 						 <button type="button" class="btn btn-md btn-block btn-outline-success">Pronto</button>
-					 </div>
-         </div>
-         <div class="card mb-4 shadow-sm">
-           <div class="card-header text-white bg-primary">
-             <h4 class="my-0 font-weight-normal">Sociedades</h4>
-           </div>
-           <div class="card-body">
-						 <h2 class="card-title pricing-card-title">Informaci&oacute;n</h2>
-						 <ul class="list-styled mt-3 mb-4">
-               <li>Informaci&oacute;n tributaria</li>
-               <li>Mercado y licitaciones</li>
-							 <li>Previsional y laboral</li>
-							 <li>Gobierno transparente</li>
-             </ul>
-           </div>
-					 <div class="card-footer bg-transparent border-primary">
-						 <button type="button" class="btn btn-md btn-block btn-outline-primary">Pronto</button>
 					 </div>
          </div>
        </div>
