9aebfb9725181b30402d83ca70f4cdd0b006ff94
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2018-12-03 10:43:23 +0000

    Fix

--- templates/cars.php
+++ templates/cars.php
@@ -23,17 +23,9 @@ if(isset($_GET['err']) && !empty($_GET['err'])) {
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <small id="emailHelp" class="form-text text-muted">Para motos agregue un 0. Ejemplo LY391 debe ser <small>LY</small>0<small>391</small>.</small>
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="button" class="btn btn-primary">Buscar</button>
 </form>
            </div>
 					 <div class="card-footer bg-transparent border-warning">
