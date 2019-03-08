b75c02b872acf616e06c3df7f5dbcf047fc1f225
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2019-03-07 22:47:28 -0800

    Fix

--- templates/login.php
+++ templates/login.php
@@ -13,12 +13,8 @@ if (isset( $identifier_session ) && ! empty( $identifier_session )) {
 		<div class="container">
 			<div class="row">
 				<div class="col-md-6 hero-text">
-					<h2>Invest in <span>Bitcoin</span> <br>Bitcoin Trading</h2>
-					<h4>Use modern progressive technologies of Bitcoin to earn money</h4>
-					<form class="hero-subscribe-from">
-						<input type="text" placeholder="Enter your email">
-						<button class="site-btn sb-gradients">Get Started</button>
-					</form>
+					<h2>Datos abiertos<br> <span>simplificado</span>.</h2>
+					<h4>Transformamos datos irrelevantes en informaci&oacute;n de valor.</h4>
 				</div>
 				<div class="col-md-6">
 					<img src="/assets/imgs/laptop.png" class="laptop-image" alt="">
@@ -29,126 +25,42 @@ if (isset( $identifier_session ) && ! empty( $identifier_session )) {
 	<!-- Hero section end -->
 
 
-	<!-- About section -->
-	<section class="about-section spad">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6 offset-lg-6 about-text">
-					<h2>What is Bitcoin</h2>
-					<h5>Bitcoin is an innovative payment network and a new kind of money.</h5>
-					<p>Bitcoin is one of the most important inventions in all of human history. For the first time ever, anyone can send or receive any amount of money with anyone else, anywhere on the planet, conveniently and without restriction. It’s the dawn of a better, more free world.</p>
-					<a href="" class="site-btn sb-gradients sbg-line mt-5">Get Started</a>
-				</div>
-			</div>
-			<div class="about-img">
-				<img src="/assets/imgs/about-img.png" alt="">
-			</div>
-		</div>
-	</section>
-	<!-- About section end -->
-
-
-	<!-- Features section -->
-	<section class="features-section spad gradient-bg">
-		<div class="container text-white">
-			<div class="section-title text-center">
-				<h2>Our Features</h2>
-				<p>Bitcoin is the simplest way to exchange money at very low cost.</p>
-			</div>
-			<div class="row">
-				<!-- feature -->
-				<div class="col-md-6 col-lg-4 feature">
-					<i class="ti-mobile"></i>
-					<div class="feature-content">
-						<h4>Mobile Apps</h4>
-						<p>Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-						<a href="" class="readmore">Readmore</a>
-					</div>
-				</div>
-				<!-- feature -->
-				<div class="col-md-6 col-lg-4 feature">
-					<i class="ti-shield"></i>
-					<div class="feature-content">
-						<h4>Safe & Secure</h4>
-						<p>Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-						<a href="" class="readmore">Readmore</a>
-					</div>
-				</div>
-				<!-- feature -->
-				<div class="col-md-6 col-lg-4 feature">
-					<i class="ti-wallet"></i>
-					<div class="feature-content">
-						<h4>Wallet</h4>
-						<p>Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-						<a href="" class="readmore">Readmore</a>
-					</div>
-				</div>
-				<!-- feature -->
-				<div class="col-md-6 col-lg-4 feature">
-					<i class="ti-headphone-alt"></i>
-					<div class="feature-content">
-						<h4>Experts Support</h4>
-						<p>Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-						<a href="" class="readmore">Readmore</a>
-					</div>
-				</div>
-				<!-- feature -->
-				<div class="col-md-6 col-lg-4 feature">
-					<i class="ti-reload"></i>
-					<div class="feature-content">
-						<h4>Instant Exchange</h4>
-						<p>Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-						<a href="" class="readmore">Readmore</a>
-					</div>
-				</div>
-				<!-- feature -->
-				<div class="col-md-6 col-lg-4 feature">
-					<i class="ti-panel"></i>
-					<div class="feature-content">
-						<h4>Recuring Buys</h4>
-						<p>Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-						<a href="" class="readmore">Readmore</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!-- Features section end -->
-
-
 	<!-- Process section -->
 	<section class="process-section spad">
 		<div class="container">
 			<div class="section-title text-center">
-				<h2>Get Started With Bitcoin</h2>
-				<p>Start learning about Bitcoin with interactive tutorials. It’s fun, easy, and takes only a few minutes! </p>
+				<h2>Cómo funciona</h2>
+				<p>Procesamos datos abiertos de fuentes públicas para generar informaci&oacute;n &uacute;til a la ciudadan&iacute;a.</p>
 			</div>
 			<div class="row">
 				<div class="col-md-4 process">
 					<div class="process-step">
 						<figure class="process-icon">
-							<img src="/assets/imgs/process-icons/1.png" alt="#">
+							<i class="fa fa-cloud-download fa-5x"></i>
 						</figure>
-						<h4>Create Your Wallet</h4>
-						<p>Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+						<br>
+						<h4>Recopilamos</h4>
+						<p>Buscamos datos abiertos disponibles bajo la Ley de Transparencia o fuentes p&uacute;blicas.</p>
 					</div>
 				</div>
 				<div class="col-md-4 process">
 					<div class="process-step">
 						<figure class="process-icon">
-							<img src="/assets/imgs/process-icons/2.png" alt="#">
+							<i class="fa fa-cogs fa-5x"></i>
 						</figure>
-						<h4>Create Your Wallet</h4>
-						<p>Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+						<br>
+						<h4>Procesamos</h4>
+						<p>Usamos "Machine Learning" para crear relaciones para enriquecer la informaci&oacute;n,</p>
 					</div>
 				</div>
 				<div class="col-md-4 process">
 					<div class="process-step">
 						<figure class="process-icon">
-							<img src="/assets/imgs/process-icons/3.png" alt="#">
+							<i class="fa fa-hand-spock-o fa-5x"></i>
 						</figure>
-						<h4>Create Your Wallet</h4>
-						<p>Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+						<br>
+						<h4>P&uacute;blicamos</h4>
+						<p>Los resultados estan disponibles para uso privado o academico. </p>
 					</div>
 				</div>
 			</div>
@@ -156,45 +68,7 @@ if (isset( $identifier_session ) && ! empty( $identifier_session )) {
 	</section>
 	<!-- Process section end -->
 
-
-	<!-- Fact section -->
-	<section class="fact-section gradient-bg">
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-6 col-md-6 col-lg-3">
-					<div class="fact">
-						<h2>60</h2>
-						<p>Support <br> Countries</p>
-						<i class="ti-basketball"></i>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-6 col-lg-3">
-					<div class="fact">
-						<h2>12K</h2>
-						<p>Transactions  <br> per hour</p>
-						<i class="ti-panel"></i>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-6 col-lg-3">
-					<div class="fact">
-						<h2>5B</h2>
-						<p>Largest <br> Transactions</p>
-						<i class="ti-stats-up"></i>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-6 col-lg-3">
-					<div class="fact">
-						<h2>240</h2>
-						<p>Years <br> of Experience</p>
-						<i class="ti-user"></i>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!-- Fact section end -->
-
-
 	<?php
+	include_once("facts.php");
 	include_once("footer.php");
 	?>
