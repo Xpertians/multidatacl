149f6c9b186bdd9e5dd0cf6db2d31588ceb7083e
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2019-03-07 16:31:54 -0800

    Adding small changes

--- config.php
+++ config.php
@@ -1,24 +1,15 @@
 <?php
-/**
- * HybridAuth
- * http://hybridauth.sourceforge.net | http://github.com/hybridauth/hybridauth
- * (c) 2009-2014, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
- */
-
-// ----------------------------------------------------------------------------------------
-//	HybridAuth Config file: http://hybridauth.sourceforge.net/userguide/Configuration.html
-// ----------------------------------------------------------------------------------------
 
 return
     [
-        "base_url"   => "http://mdata.opendatacollector.com/hybrid.php",
+        "base_url"   => "http://localhost:8000/hybrid.php",
         "providers"  => [
             "Google"   => [
                 "enabled" => true,
                 "keys"    => [
-                    "id" => "204025247100-7lefpfc4arp25fkfffnj2hg2bbd9imuj.apps.googleusercontent.com",
-                    "secret" => "pEiZ1DtkxUjWKiudNSQFLouu"
-                    ],
+                  "id" => "204025247100-7lefpfc4arp25fkfffnj2hg2bbd9imuj.apps.googleusercontent.com",
+                  "secret" => "pEiZ1DtkxUjWKiudNSQFLouu"
+                ],
             ],
             "Facebook" => [
                 "enabled"        => false,
@@ -29,12 +20,19 @@ return
                 "enabled" => false,
                 "keys"    => [ "key" => "", "secret" => "" ]
             ],
+            "LinkedIn" => [
+                "enabled" => false,
+                "keys"    => [ "key" => "", "secret" => "" ]
+            ],
+            "Github"   => array(
+                "enabled" => true,
+                "keys"    => array(
+                    "id"     => "7fc2a3116c86b9dac673",
+                    "secret" => "1a3a1ba0c158a3e0cd1538b4f3c36fd6e6c10556"
+                ),
+                "wrapper" => [ "path" => "providers/GitHub.php", "class" => "Hybrid_Providers_GitHub" ]
+            ),
         ],
-        // If you want to enable logging, set 'debug_mode' to true.
-        // You can also set it to
-        // - "error" To log only error messages. Useful in production
-        // - "info" To log info and error messages (ignore debug messages)
-        "debug_mode" => "false",
-        // Path to file writable by the web server. Required if 'debug_mode' is not false
+        "debug_mode" => false,
         "debug_file" => "bug.txt",
     ];
--- mdata.sql
+++ mdata.sql
@@ -1,10 +1,38 @@
-CREATE TABLE IF NOT EXISTS `users` (
-`id` int(10) NOT NULL AUTO_INCREMENT,
-PRIMARY KEY  (id),
-  `identifier` varchar(50) NOT NULL,
-UNIQUE KEY `identifier` (`identifier`),
-  `email` varchar(50) DEFAULT NULL,
-  `first_name` varchar(20) DEFAULT NULL,
-  `last_name` varchar(20) DEFAULT NULL,
-  `avatar_url` varchar(255)
-) ENGINE=InnoDB;
+<?php
+
+return
+    [
+        "base_url"   => "http://localhost:8000/hybrid.php",
+        "providers"  => [
+            "Google"   => [
+                "enabled" => true,
+                "keys"    => [
+                  "id" => "204025247100-7lefpfc4arp25fkfffnj2hg2bbd9imuj.apps.googleusercontent.com",
+                  "secret" => "pEiZ1DtkxUjWKiudNSQFLouu"
+                ],
+            ],
+            "Facebook" => [
+                "enabled"        => false,
+                "keys"           => [ "id" => "", "secret" => "" ],
+                "trustForwarded" => false
+            ],
+            "Twitter"  => [
+                "enabled" => false,
+                "keys"    => [ "key" => "", "secret" => "" ]
+            ],
+            "LinkedIn" => [
+                "enabled" => false,
+                "keys"    => [ "key" => "", "secret" => "" ]
+            ],
+            "Github"   => array(
+                "enabled" => true,
+                "keys"    => array(
+                    "id"     => "7fc2a3116c86b9dac673",
+                    "secret" => "1a3a1ba0c158a3e0cd1538b4f3c36fd6e6c10556"
+                ),
+                "wrapper" => [ "path" => "providers/GitHub.php", "class" => "Hybrid_Providers_GitHub" ]
+            ),
+        ],
+        "debug_mode" => false,
+        "debug_file" => "bug.txt",
+    ];
