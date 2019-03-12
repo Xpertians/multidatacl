93f5c4aee3824e2e2be1be22e314c6b469159920
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2019-03-12 15:31:55 -0700

    Trying grid

--- templates/home.php
+++ templates/home.php
@@ -45,15 +45,13 @@ if (isset( $identifier_session ) && ! empty( $identifier_session )) {
 							</div>
 						</div>
 
-						<h4>Detalles</h4>
-
+						<h4>DETALLES</h4>
 						<table class="table table-bordered">
 						  <thead>
 						    <tr>
 						      <th scope="col">#</th>
 						      <th scope="col">First</th>
 						      <th scope="col">Last</th>
-						      <th scope="col">Handle</th>
 						    </tr>
 						  </thead>
 						  <tbody>
@@ -61,18 +59,15 @@ if (isset( $identifier_session ) && ! empty( $identifier_session )) {
 						      <th scope="row">1</th>
 						      <td>Mark</td>
 						      <td>Otto</td>
-						      <td>@mdo</td>
 						    </tr>
 						    <tr>
 						      <th scope="row">2</th>
 						      <td>Jacob</td>
 						      <td>Thornton</td>
-						      <td>@fat</td>
 						    </tr>
 						    <tr>
 						      <th scope="row">3</th>
 						      <td colspan="2">Larry the Bird</td>
-						      <td>@twitter</td>
 						    </tr>
 						  </tbody>
 						</table>
