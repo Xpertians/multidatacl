838786a2ee9f32ce3b29cfa9f8c04fcc4f242b31
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2019-04-20 23:27:24 -0700

    Adding cars

--- index.php
+++ index.php
@@ -118,7 +118,7 @@ $app->get( '/home/', $authenticate( $app ), function () use ( $app, $model ) {
     }
 );
 
-$app->get( '/home/:qry', function ( $qry ) use ( $app, $model ) {
+$app->get( '/home/:qry', $authenticate( $app ), function ( $qry ) use ( $app, $model ) {
         switch($qry){
             case "cars":
                  $app->render( 'cars.php', [ 'model' => $model ] );
