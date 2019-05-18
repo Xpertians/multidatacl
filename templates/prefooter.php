290162d2b5e6b68136586b2f5ecf72418902e7a3
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2019-05-18 15:14:40 -0700

    Adding prefooter links

--- templates/prefooter.php
+++ templates/prefooter.php
@@ -1,6 +1,7 @@
 <!-- PREFOOTER -->
 <footer class="container">
     <hr class="featurette-divider">
+    <div class="d-none d-lg-block">
     <p class="float-right">
         <small>Fuente de datos: <br>
             <a href="https://OpenDataCollector.com" target="_blank">
@@ -8,8 +9,9 @@
             </a>
         </small>
     </p>
+    </div>
     <p>
-        &copy; <?php echo date('Y'); ?> Xpertians &middot; 
+        &copy; <?php echo date('Y'); ?> Xpertians &middot;
         <a href="https://odc.xpertians.com/docs/terminos-de-servicio/" target="_blank">T&eacute;rminos de Uso</a> &middot; <a href="https://github.com/Xpertians/odc/issues/new" target="_blank">Ayuda</a>
     </p>
 </footer>
