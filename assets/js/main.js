ec9b42e6a7537c8d96e709cb4226b500ea1dcaf2
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2019-05-20 00:33:05 -0700

    Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,5 @@
-var specialKeys = new Array();
-specialKeys.push(8);  //Backspace
-specialKeys.push(9);  //Tab
-specialKeys.push(46); //Delete
-specialKeys.push(36); //Home
-specialKeys.push(35); //End
-specialKeys.push(37); //Left
-specialKeys.push(39); //Right
-
 function IsAlphaNumeric(e) {
    var keyCode = e.keyCode == 0 ? e.charCode : e.keyCode;
    var ret = ((keyCode >= 48 && keyCode <= 57) || (keyCode >= 65 && keyCode <= 90) || keyCode == 32 || (keyCode >= 97 && keyCode <= 122));
-   //document.getElementById("error").style.display = ret ? "none" : "inline";
    return ret;
 }
