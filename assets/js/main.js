aef1ca9d9fd2c421c247a3e8f9869975d8a5a58d
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2019-05-20 00:37:29 -0700

    Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,9 @@ function IsAlphaNumeric(e) {
    var ret = ((keyCode >= 48 && keyCode <= 57) || (keyCode >= 65 && keyCode <= 90) || keyCode == 32 || (keyCode >= 97 && keyCode <= 122));
    return ret;
 }
+
+$(function() {
+    $('#qryValue').keyup(function() {
+        this.value = this.value.toUpperCase();
+    });
+});
