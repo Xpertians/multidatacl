30896bed077c52014ea9e6f443ae88608a24e282
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2019-05-20 00:35:05 -0700

    Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 function IsAlphaNumeric(e) {
    var keyCode = e.keyCode == 0 ? e.charCode : e.keyCode;
    var ret = ((keyCode >= 48 && keyCode <= 57) || (keyCode >= 65 && keyCode <= 90) || keyCode == 32 || (keyCode >= 97 && keyCode <= 122));
-   return ret.toUpperCase();
+   return ret;
 }
