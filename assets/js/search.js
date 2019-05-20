47d78ec2957a4f3402ed9b4909cbbc5740aaf0ef
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2019-05-20 00:23:12 -0700

    Fix

--- assets/js/search.js
+++ assets/js/search.js
@@ -29,10 +29,16 @@ $(function() {
 });
 
 
-$(function() {
-    $('input.alpha[$id=qryValue]').keyup(function() {
-        if (this.value.match(/[^a-zA-Z0-9 ]/g)) {
-            this.value = this.value.replace(/[^a-zA-Z0-9 ]/g, '');
-        }
-    });
+$('qryValue').bind('keypress', function(e) {// Kami.2013.12.24.10:57 Allow only letters, numbers, and @ , . - _
+var allowedCode = [8, 13, 32, 44, 45, 46, 95];
+var charCode = (e.charCode) ? e.charCode : ((e.keyCode) ? e.keyCode :
+	  ((e.which) ? e.which : 0));
+ if (charCode > 31 && (charCode < 64 || charCode > 90) &&
+	(charCode < 97 || charCode > 122) &&
+	(charCode < 48 || charCode > 57) &&
+	(allowedCode.indexOf(charCode) == -1)) {
+	e.preventDefault();
+	$('.alert-box.onerror').html('<h3>ALLOW CHARACTERS: a..z A..Z 0..9 - _ . , @</h3>').fadeIn();
+	return false;
+ }
 });
