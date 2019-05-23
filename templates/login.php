ac61dd6dfac4b1eb13a9112c0f5008d16782daf6
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2019-05-22 23:24:58 -0700

    Adding warning msg

--- templates/login.php
+++ templates/login.php
@@ -18,6 +18,7 @@ include_once("header.php");
       <img src="/assets/imgs/google.png" height=20px> Ingresar con <b>Google</b>
     </a>
     <hr>
+    <center><font color=red>A partir del 20 de Mayo del 2019, MultiDATA no requiere crear una cuenta especial. <br>Desde ahora, solo necesitas una cuenta en una de las redes sociales listadas m&aacute;s arriba. Este cambio busca facilitar el accesso, mantener los datos de nuestros usuarios como privados al no contar con un registro de cuentas, y simplificar el funcionamiento de la web. Adem&aacute;s desde ahora puedes descargar el c&oacute;digo fuente de MultiDATA desde GitHub</font></center>
     <button class="btn btn-xs btn-outline-danger" type="button" onClick="window.location.href='/';">&laquo; volver</button>
     <p class="mt-5 mb-3 text-muted">&copy; <?php echo date('Y'); ?> Xpertians</p>
 </form>
