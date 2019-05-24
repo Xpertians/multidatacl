8c30fe75bc77193ba2028505364f505f17a86045
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2019-05-23 20:04:39 -0700

    Small fix tot he btns

--- templates/login.php
+++ templates/login.php
@@ -13,19 +13,20 @@ include_once("header.php");
         MultiDATA no requiere registro, solo necesitas una cuenta en una red social.
     </p>
     <p align='center'>
-      <a href="/login/facebook" class="btn btn-outline-primary">
+      <a href="/login/facebook" class="btn btn-outline-primary btn-block">
         <img src="/assets/imgs/facebook.png" width="20"> Ingresar con <b>Facebook</b>
       </a>
     </p>
     <p align='center'>
-      <a href="/login/github" class="btn btn-outline-secondary">
+      <a href="/login/github" class="btn btn-outline-secondary btn-block">
         <img src="/assets/imgs/github.png" width="20"> Ingresar con <b>GitHub</b>
       </a>
     </p>
     <p align='center'>
-      <a href="/login/google" class="btn btn-primary">
+      <a href="/login/google" class="btn btn-primary btn-block">
         <img src="/assets/imgs/google.png" width="20"> Ingresar con <b>Google</b>
       </a>
+      <br>
       <small>
           <font color=red>
               Este sitio web esta en proceso de verificaci&oacute;n por Google, por lo que se muestra un mensaje de error o de advertencia de seguridad. Es NORMAL, ser&aacute; eliminado una vez que Google complete el proceso.
